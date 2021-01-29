b31d5e7de6a779a3b6131171924ad2c6200153f8
Robzlegz <robzlegz@gmail.com>
2021-01-29 18:58:12 +0200

    subtotal styling

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 const Subtotal = () => {
+
+    const [{basket}, dispatch] = useStateValue();
+
     return (
-        <div className="subtotal">
-            
+        <div className="subtotal-container">
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <p>Total ({basket?.length} items): <strong>{value}</strong></p>
+                    </>
+                )}
+                decimalScale={2}
+                value={getBasketTotal(basket)} 
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
+            <button className="checkout-button">Proceed to Checkout</button>
         </div>
     );
 }
