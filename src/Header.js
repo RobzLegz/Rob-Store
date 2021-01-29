b9f1a676374ef6684df278f311abb41f34beef99
Robzlegz <robzlegz@gmail.com>
2021-01-29 14:40:31 +0200

    uploading fonts

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
     return (
         <div className="header">
-            
+            <h1 className="logo">Rob Store</h1>
         </div>
     );
 }
