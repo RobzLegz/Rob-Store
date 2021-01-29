37efa5b8a651682674152c5429681d4292a6fc1b
Robzlegz <robzlegz@gmail.com>
2021-01-29 14:59:26 +0200

    before bugs

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import "./Header.css";
 
+
 const Header = () => {
     return (
         <div className="header">
             <h1 className="logo">Rob Store</h1>
+            <div className="header-boxes">
+                <div className="header-box">
+                    <small>Hello Guest</small>
+                    <p>Login</p>
+                </div>
+                <div className="header-box">
+                    <small>Cart</small>
+                    <div className="cart-container">
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
