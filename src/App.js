a3517796411546083eb0e1040e0e31cb4df0fc24
Robzlegz <robzlegz@gmail.com>
2021-01-29 13:41:12 +0200

    creating components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./App.css";
+import Header from './Header';
+import Home from './Home';
 
 const App = () => {
   return (
     <div>
-      ecomerce
+      <Header />
+      <Home />
     </div>
   );
 }
